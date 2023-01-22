5799daef0853046ffd48ec1d099e7298688cb711
tonayam <tonayam99@gmail.com>
2023-01-22 19:02:43 +0100

    The Game and Multi-Chain-Economy Sections Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <>
@@ -11,6 +11,8 @@ export default function Home() {
       </Head>
       <main>
         <Header />
+        <TheGame />
+        <MultiChainEconomy />
       </main>
     </>
   );
@@ -29,5 +31,47 @@ const Header = () => {
         <button className='green'>Play game</button>
       </a>
     </header>
+  );
+};
+
+const TheGame = () => {
+  return (
+    <section className='the-game'>
+      <h2 className='section-title'>THE GAME</h2>
+      <div className='blocks'>
+        <div className='block img'></div>
+        <div className='block'>
+          <p>
+            ChessOnchain is a play to earn digital chess game where players can
+            collect NFTs and compete in multichain/interchain chess competitions
+            and 1v1 matches.
+          </p>
+          <a href='#'>
+            <button className='green'>Learn More</button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const MultiChainEconomy = () => {
+  return (
+    <section className='multi-chain-economy'>
+      <h2 className='section-title'>MULTI-CHAIN ECONOMY</h2>
+      <div className='blocks'>
+        <div className='block'>
+          <p>
+            The idea for a simple economy among multiple blockchain ecosystems
+            for competitions and on-chain records as a data store is useful to
+            the blockchain and gaming markets as competitions help improve
+            market stat, it would boost and improve areas like the global chess
+            market, the blockchain and multi-chain economy since multi-chain is
+            the desired future of the blockchain economy.
+          </p>
+        </div>
+        <div className='block img'></div>
+      </div>
+    </section>
   );
 };
