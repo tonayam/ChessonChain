d1acde50c3ee88885808d8e66a713205657150ea
Austin Tonayam <tonayam99@gmail.com>
2023-08-21 14:35:35 +0100

    Updates

--- context/context.jsx
+++ context/context.jsx
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const baseURL = `  https://99ca-197-211-58-194.ngrok-free.app/api/v1`;
+  const baseURL = `https://chessonchain-waitlist-api.onrender.com/api/v1`;
   const [showNavbar, setShowNavbar] = useState(false);
   const [lightMode, setLightMode] = useState(false);
 
